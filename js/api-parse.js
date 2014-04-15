b6212b37db75280378a65fd9c4589c1e28794c51
Brian Boadi <geezeupp@gmail.com>
2014-04-15 10:49:42 +0200

    [FIX] Re-nettoyage du code

--- js/api-parse.js
+++ js/api-parse.js
@@ -1,1 +1,31 @@
+var headers = {"X-Parse-Application-Id":"gn23tgrg2J5EYgYJJqUW7tvlqCRRbULZZjuCv1de", "X-Parse-REST-API-Key":"ZblCesxlIR6w5TI6IumInVUJrhLDTB19cC6xsTeF"};
+
+
+//send notification to all users in category
+function sendPush(){
+	
+	var title =  document.getElementById("title").value;
+	var channel = $('#category option:selected').val();
+
+	// Variable to store data:
+	var pushData = '{ "channels": ["'+channel+'"], "data": {"alert": "'+title+'" }}' ;
+	
+		$.ajax({
+				type: "POST",
+				url: "https://api.parse.com/1/push",
+				data: pushData,
+				contentType: "application/json; charset=utf-8",
+			    dataType: "json",
+				headers:headers,
+				success: function (data, status, jqXHR) {
+						alert("Votre notification a été envoyée");
+						savePushNotification(title,channel);
+						 },
+					 
+				error: function (jqXHR, status) {            
+							 
+						}
+				 });
+		
+}
 
