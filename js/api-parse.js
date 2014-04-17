a81f8b4cc5f9244aea986010bc64c8870d14d056
Brian Boadi <geezeupp@gmail.com>
2014-04-17 18:43:41 +0200

    [ADD] Page sport + communication webview <--> Android pour inscription/desinscription

--- js/api-parse.js
+++ js/api-parse.js
@@ -29,3 +29,108 @@ function sendPush(){
 		
 }
 
+
+function getAllNotificationsForChannel(channel){
+	
+	$.ajax({
+		type: "GET",
+		url: 'https://api.parse.com/1/classes/push?where={"channel":"'+channel+'"}',
+		contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+		headers:headers,
+		success: function (data, status, jqXHR) {
+			
+			buildNotificationList(data);        
+		},
+			 
+		error: function (jqXHR, status) {            
+				alert('error');	 
+				}
+		 });
+}
+
+
+//Build list of notifications for channel
+function buildNotificationList(data){
+	$("#list-push").empty();
+	for(var i in data.results){
+		var objectID=data.results[i].objectId;
+		var link=addParameter('news.html','objectID',objectID);
+		var push = '<a href="'+link+'" rel="external" class="list-group-item"> <h4 class="list-group-item-heading">'+data.results[i].title+'</h4><span class="glyphicon glyphicon-chevron-right pull-right"></span><p class="list-group-item-text">'+data.results[i].message.substring(0,50)+'</p> </a>';
+		$("#list-push").append(push);
+	}	 
+	$('#list-push').show();
+}
+
+function getFullMessage(objectID){
+	$.ajax({
+		type: "GET",
+		url: 'https://api.parse.com/1/classes/push?where={"objectId":"'+objectID+'"}',
+		contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+		headers:headers,
+		success: function (data, status, jqXHR) {
+			
+			buildFullMessage(data);        
+		},
+			 
+		error: function (jqXHR, status) {            
+				alert('error');	 
+				}
+		 });
+
+}
+
+function buildFullMessage(data){
+	
+	$("#full-message").empty();
+	
+	for(var i in data.results){
+		var message = '<div class="panel-heading">';
+		message+= '<h5 class = panel-Title>'+data.results[i].title+'</h5></div> ';
+		message+='<div class="panel-body">'+data.results[i].message +'</div>';
+		message+='<div class = panel-footer><center><strong>'+data.results[i].channel+'</strong></center></div></div>';		
+		$("#full-message").append(message);
+	}
+	$('#full-message').show();
+}
+
+function subscribeOrUnsubscribeFromChannel(Id,channel){
+	$.ajax({
+		type: "GET",
+		url: 'https://api.parse.com/1/installations/'+Id,
+		contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+		headers:headers,
+		success: function (data, status, jqXHR) {
+			buildCorrectButton(data,channel);
+		},
+			 
+		error: function (jqXHR, status) {            
+				alert('errorstats');	 
+				}
+		 });
+	
+}
+
+function buildCorrectButton(data,channel){
+	
+	var button='<input type="button" class="btn btn-success" value="inscription" onClick="subscribeToChannel(\''+channel+'\')" />';
+	if(data.channels != undefined){
+		var channelsArray = data.channels;
+		for (var i = 0; i < channelsArray.length; i++) {
+		    if (channelsArray[i] == channel ){
+		    	
+		    	button='<input type="button" class="btn btn-danger" value="desinscription" onClick="unsubscribeFromChannel(\''+channel+'\')" />';
+		    	
+		    	  break;
+		    }
+		}
+	}
+		
+	
+	$("#btn-parametre").empty();
+	$("#btn-parametre").append(button);
+	$("#btn-parametre").show();
+	
+}
---
+++ js/utils.js
@@ -0,0 +1,124 @@
+	function extractUrlParams() {
+		var urlParams = location.search.substring(1).split('&');
+		var tabParams = [];
+		for ( var i = 0; i < urlParams.length; i++) {
+			var param = urlParams[i].split('=');
+			tabParams[param[0]] = param[1];
+		}
+		return tabParams;
+
+	}
+	
+	function getParameterValue(name) {
+		var tabParams = extractUrlParams();
+		var value = "All"; 
+		if (tabParams[name] != null) {
+			value = tabParams[name];
+		}
+		return value;
+
+	}
+	
+	function getLink(page,parameterName) {
+		var link = page + '?'+parameterName+'='+getParameterValue(parameterName);
+		document.location.href = link;
+	}
+	
+	function addParameter(page,parameterName,value){
+		var link = page;
+		
+		if(page.indexOf('?') === -1){
+			link = page + '?'+parameterName+'='+value;
+		}else{
+			link = page + '&'+parameterName+'='+value;
+		}
+		
+		return link;
+	}
+	
+
+ 
+    $(document).ready(function(){ 
+    	//Naviguation entre les différents onglets
+        $('a').click(function (e) {
+        	e.preventDefault();
+        	$(this).tab('show');	    	
+        });
+        
+    	$("#mentions_legales").click(function () {     
+    	    document.location.href = "mentions_legales.html";
+    	});
+    	
+    	$("#retour").click(function () {
+    		//retour vers la rubrique d'où l'on vient
+    	    document.location.href = "javascript:history.go(-1)";
+    	});
+    	
+    	$("#a_propos").click(function () {     
+    	    document.location.href = "a_propos.html";
+    	});
+    	
+    	$("#list-push").click(function () {     
+    	    document.location.href = "news.html";
+    	});
+    	
+    	$("#accueil").click(function () {  
+    	     getLink('index.html','uid');
+    	});
+    	
+    	$("#site_brdx").click(function () {     
+    	    document.location.href = "http://www.u-bordeaux1.fr/";
+    	});
+    	
+    	$("#site_ent").click(function () {     
+    	    document.location.href = "http://www.u-bordeaux.fr/Vie-des-campus/Services-numeriques/Identifiants-ENT";
+    	});	 
+    	
+    	
+    	$("#site_amb").click(function () {     
+    	    document.location.href = "http://asso.miagebordeaux.fr";
+    	});
+    	
+    	$("#site_interasso").click(function () {     
+    	    document.location.href = "https://fr-fr.facebook.com/interassoselanassociatif";
+    	});
+    		    	
+    	$("#site_mtech").click(function () {     
+    	    document.location.href = "http://mtech.bdx1.free.fr";
+    	});
+
+    	$("#site_jumbo").click(function () {     
+    	    document.location.href = "http://jumbo.miagebordeaux.fr";
+    	});	
+    	
+    	$("#site_ru").click(function () {     
+    	    document.location.href = "https://www.u-bordeaux.fr/Vie-des-campus/Vie-pratique/Restauration2";
+    	});
+    	
+    	$("#site_sport").click(function () {     
+    	    document.location.href = "http://www.u-bordeaux1.fr/vie-etudiante/culture-sport-loisirs/activites-sportives.html ";
+    	});
+    		    	
+    });
+    
+    function goToPage(page){
+    	document.location.href = "."+page;
+    }
+    
+    function showAndroidToast(toast) {
+    	Android.showToast(toast);
+    }
+    
+    function subscribeToChannel(channel) {
+    	Android.subscribeToChannel(channel);
+    }
+    
+    function unsubscribeFromChannel(channel) {
+    	Android.unsubscribeFromChannel(channel);
+    }
+    
+    
+    
+    
+	 
+	
