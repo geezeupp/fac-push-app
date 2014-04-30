983d7330d9118b151f2a063a62641a77b241ae5f
Brian Boadi <geezeupp@gmail.com>
2014-04-30 09:58:49 +0200

    FIX- Encodage

--- js/api-parse.js
+++ js/api-parse.js
@@ -89,7 +89,7 @@ function buildFullMessage(data){
 		var message = '<div class="panel-heading">';
 		message+= '<h5 class = panel-Title>'+data.results[i].title+'</h5></div> ';
 		message+='<div class="panel-body">'+data.results[i].message +'</div>';
-		message+='<div class = panel-footer><center><strong>'+'Envoyé le '+data.results[i].updatedAt.substring(0,10)+' à '+data.results[i].updatedAt.substring(11,19)+'</strong></center></div></div>';		
+		message+='<div class = panel-footer><center><strong>'+'Envoy\351 le '+data.results[i].updatedAt.substring(0,10)+' \340 '+data.results[i].updatedAt.substring(11,19)+'</strong></center></div></div>';		
 		$("#full-message").append(message);
 	}
 	$('#full-message').show();
